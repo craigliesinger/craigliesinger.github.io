12ac44b1925b3ecc25faec425e8fb2a6436a66d0
craigliesinger <craig.liesinger@gmail.com>
2016-04-20 20:00:40 +0200

    Update app.js
    image4

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ myProjects.controller('ProjectsCtrl', ['$scope', function ($scope) {
 	 'time': '2016',
      'overview': "Age badges to mark every age milestone of your little one!", 
 	 'detail': "Babies grow up fast!  Capture every step with a Baby Badge.  Don't spend $10+ for 12 paper baby stickers when you can create hundreds of combinations for any day, week, month or year milestone for free with Baby Badge.",
-	 'image': [{'ss': "img/bb/website1.jpg"}, {'ss': "img/bb/website2.jpg"}, {'ss': "img/bb/website3.jpg"}],
+	 'image': [{'ss': "img/bb/website1.jpg"}, {'ss': "img/bb/website2.jpg"}, {'ss': "img/bb/website3.jpg"}, {'ss': "img/bb/website4.jpg"}],
 	 'icon': "img/bb/appIcon2_3x180.png",
 	 'link': "https://itunes.apple.com/vn/app/math-espresso/id972446911?mt=8"
 	 }
