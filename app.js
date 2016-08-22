1557b7661bdf8bac23102ba060cc1ddc8ad27e25
craigliesinger <craig.liesinger@gmail.com>
2016-08-22 10:57:59 +0200

    adding bled image to content

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ myProjects.controller('ProjectsCtrl', ['$scope', function ($scope) {
 	 'time': 'Aug 18th, 2016',
      'overview': "Arduino + Red Bear BLE 2.1 + Swift = Cool",
 	 'detail': "I love the thought of manipulating the outside world with code.  This project uses Arduino, Red Bear BLE Shield and iPhone (Swift) to control the color and brightness of a light.",
-	 'image': [{'header': "Over 150 irregular verbs",'ss': "img/gsv/iphone_ss1.png"},{'header': "All cases and tenses", 'ss': "img/gsv/iphone_ss2.png"}, {'header': "Search feature for fast verb finding", 'ss': "img/gsv/iphone_ss3.png"}]
+	 'image': "img/hack/DIY_bled.jpeg"
 	 }
   ];  
   
